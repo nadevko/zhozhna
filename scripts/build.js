46b90faf9d6bea621f7cb426ba1c11759a2cd2cc
Nadeŭka <nadevko@riseup.net>
2022-08-19 14:02:15 +0300

    Add precommit fix

--- scripts/build.js
+++ scripts/build.js
@@ -1,4 +1,4 @@
-'use strict';
+#!/usr/bin/env node
 
 // Do this as the first thing so that any code reading it knows the right env.
 process.env.BABEL_ENV = 'production';
@@ -150,7 +150,7 @@ function build(previousFileSizes) {
         if (Object.prototype.hasOwnProperty.call(err, 'postcssNode')) {
           errMessage +=
             '\nCompileError: Begins at CSS selector ' +
-            err['postcssNode'].selector;
+            err.postcssNode.selector;
         }
 
         messages = formatWebpackMessages({
---
+++ scripts/prepare.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+npx husky install
